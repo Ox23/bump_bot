155668d6c5e32445bf2c823204a359f503d845d1
0x23 <76412040+ayexyeah@users.noreply.github.com>
2021-06-06 21:51:49 -0400

    Update bump_bot.py

--- bump_bot.py
+++ bump_bot.py
@@ -3,7 +3,7 @@ import asyncio
 from discord.ext import commands, tasks
 import time
 
-token = ""
+token = ""#your account token here
 
 client = commands.Bot("$", self_bot=True)
 
@@ -12,7 +12,7 @@ async def on_connect():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    channel = client.get_channel()
+    channel = client.get_channel()#your channel id here
     while True:
         await channel.send('!d bump')
         print(f'bumped')
